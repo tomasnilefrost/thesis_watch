494afb0ac685ce873f06db5b168b7e1bf221c700
Tomas Frostensson <tomas@frostensson.com>
2012-04-24 21:39:53 +0200

    more functionality, create a dict with info

--- thesis_watch.py
+++ thesis_watch.py
@@ -10,6 +10,7 @@ import sys
 
 http = httplib2.Http()
 url = 'http://www.ida.liu.se/edu/ugrad/thesis/presentations.shtml'
+url = 'http://www.isy.liu.se/edu/pres/'
 try:
 	resp, content = http.request(url)
 except:
@@ -22,9 +23,14 @@ table = main_div.find('table')
 tr = table.findAll('tr')
 
 for i in range(0, len(tr) , 2):
-	area = tr[i].find('td').find('b').renderContents()
+	header = tr[i].find('td').find('b').renderContents()
+	header = header.split(' - ') # first element is date, second element is area
 	info = tr[i + 1].find('td')
 	title = info.find('b').renderContents()
-	info = info.find('font').renderContents()
-	info = info.replace('</br>', '').replace('<br/>', '');
-	print info
+	info = info.find('font').renderContents().replace('</br>', '').replace('<br/>', '').split('<br>')
+	info = [x.strip() for x in info]
+	date = info[2].split(', ')
+	info[2] = date[1];
+	date[1] = header[0];
+	entry = { 'date' : date[1] + ', ' + date[0], 'location' : info[2], 'title' : title, 'author' : info[0], 'level' : info[1], 'area' : header[1] }
+	print entry
