d739d54ca877eb499cc5881396c429537cde00e0
Tomas Frostensson <tomas@frostensson.com>
2012-04-25 08:08:31 +0200

    adding theses to a dict

--- thesis_watch.py
+++ thesis_watch.py
@@ -9,28 +9,28 @@ import string
 import sys
 
 http = httplib2.Http()
-url = 'http://www.ida.liu.se/edu/ugrad/thesis/presentations.shtml'
-url = 'http://www.isy.liu.se/edu/pres/'
-try:
-	resp, content = http.request(url)
-except:
-	sys.exit(1)
-"b", { "class" : "lime" }
+urls = ['http://www.ida.liu.se/edu/ugrad/thesis/presentations.shtml', 'http://www.isy.liu.se/edu/pres/']
+thesis_dict = {}
+for url in urls:
+	try:
+		resp, content = http.request(url)
+	except:
+		sys.exit(1)
 
-main_div =  BeautifulSoup(content).find('div', { 'id' : 'maincontent' })
-table = main_div.find('table')
+	main_div =  BeautifulSoup(content).find('div', { 'id' : 'maincontent' })
+	table = main_div.find('table')
+	tr = table.findAll('tr')
+	for i in range(0, len(tr) , 2):
+		header = tr[i].find('td').find('b').renderContents()
+		date, area = header.split(' - ') # first element is date, second element is area
+		info = tr[i + 1].find('td')
+		title = info.find('b').renderContents().strip()
+		author, level, info = info.find('font').renderContents().replace('</br>', '').replace('<br/>', '').split('<br>')
+		author = author.strip()
+		level = level.strip()
+		info = info.strip()
+		clock, location = info.split(', ')
+		entry = { 'date' : date + ', ' + clock, 'location' : location, 'title' : title, 'level' : level, 'area' : area }
+		thesis_dict.update({ author : entry })
 
-tr = table.findAll('tr')
-
-for i in range(0, len(tr) , 2):
-	header = tr[i].find('td').find('b').renderContents()
-	header = header.split(' - ') # first element is date, second element is area
-	info = tr[i + 1].find('td')
-	title = info.find('b').renderContents()
-	info = info.find('font').renderContents().replace('</br>', '').replace('<br/>', '').split('<br>')
-	info = [x.strip() for x in info]
-	date = info[2].split(', ')
-	info[2] = date[1];
-	date[1] = header[0];
-	entry = { 'date' : date[1] + ', ' + date[0], 'location' : info[2], 'title' : title, 'author' : info[0], 'level' : info[1], 'area' : header[1] }
-	print entry
+print thesis_dict
